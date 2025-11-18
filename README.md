479a28263e614040ea13a9b35247db5e9f2b7ede
dung2711 <taubando@gmai.com>
2025-11-18 20:52:21 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# telegram-manager-app
---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
